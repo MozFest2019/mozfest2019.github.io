a6a32b40bb761ef5ea7ed14c179510c553f4872f
MozFest2019 <bridportraspberryjam@gmail.com>
2019-10-24 21:14:15 +0100

    update --all

---
+++ Setup_Script.sh
@@ -0,0 +1,4 @@
+git config --global user.name "MozFest2019"
+git config --global user.email "bridportraspberryjam@gmail.com"
+git clone https://github.com/mozfest2019/mozfest2019.github.io
+echo "Now, type: cd mozfest2019.github.io"
